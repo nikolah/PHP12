dcefa38d3912eeb4d5ba6c206f7163c922aaf0dc
unknown <n.d.hrcic@gmail.com>
2013-05-07 19:20:38 +0200

    Funkcija provjerava postojanje kljuca i ispisuje vrijednost

---
+++ polaznici/nikolah/provjeraKljuca.php
@@ -0,0 +1,26 @@
+﻿Zadatak:
+<br>
+Napišite funkciju koja vraća vrijednost ključa ukoliko:<br>
+	- ključ postoji <br>
+	- ima vrijednost <br>
+	
+<?php	
+	function vrijednostKljuca($array, $key) {
+		if (array_key_exists($key, $array)) {
+			if(isset($array[$key])) {
+				return $array[$key];			
+			}
+		}
+		return false;
+	}
+	
+	$pizza = array (
+		'šunka' => 3,
+		'sir' => 5,
+		'gljive' => 4,
+		'sos' => 5
+		);
+	
+	echo vrijednostKljuca($pizza, 'sir');
+
+?>
